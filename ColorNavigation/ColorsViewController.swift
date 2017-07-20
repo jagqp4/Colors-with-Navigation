c961338c2dabd336ce712b7c03e21e44c58a77d5
Jordan Gunter <jordangunter@Jordans-MacBook-Pro.local>
2017-07-20 15:37:29 -0500

    initial commit

---
+++ ColorNavigation/ColorsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ColorsViewController.swift
+//  ColorNavigation
+//
+//  Created by Jordan Gunter on 7/20/17.
+//  Copyright © 2017 Jordan Gunter. All rights reserved.
+//
+
+import UIKit
+
+class ColorsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
